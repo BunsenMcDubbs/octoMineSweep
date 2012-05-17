7da0e284615d90373d5f24c8fd0f8edf644d01f7
Andrew Dai <theman@andrewdai.co>
2012-05-17 14:51:13 -0400

    added new gui test into the test package

---
+++ src/test/Gui.java
@@ -0,0 +1,15 @@
+package test;
+
+import backend.*;
+import frontend.*;
+
+public class Gui {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MineFrame mF = new MineFrame();
+	}
+
+}
