e1c9c34f0eb84d3e446e54cfdd322757f2c93567
Andrew Dai <theman@andrewdai.co>
2012-05-18 21:08:55 -0400

    added code to programically win the game to test the winning result of a game

--- src/test/Gui.java
+++ src/test/Gui.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.awt.Component;
+
+import javax.swing.Timer;
+
 import backend.*;
 import frontend.*;
 
@@ -10,6 +14,12 @@ public class Gui {
 	 */
 	public static void main(String[] args) {
 		MineFrame mF = new MineFrame();
+		mF.setDifficulty(1);
+		
+		for( Component c : mF.getField().getComponents())
+			if(!((MineSpot)c).getSpot().isBomb())
+				((MineSpot)c).getSpot().open();
+		
 		System.out.println(mF.getGame().testString());
 	}
 
