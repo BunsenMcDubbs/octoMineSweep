81d11306fe5d876d7f99864540a11093a16a5ff4
Andrew Dai <theman@andrewdai.co>
2012-05-26 21:05:55 -0400

    adding highscore functionality to the game 	new file:   src/backend/highscore/HighscoreIndex.java 	new file:   src/backend/highscore/HighscoreItem.java 	new file:   src/frontend/highscoreDisplay/HighscoreList.java

---
+++ src/backend/highscore/HighscoreItem.java
@@ -0,0 +1,5 @@
+package backend.highscore;
+
+public class HighscoreItem {
+
+}
---
+++ src/frontend/highscoreDisplay/HighscoreList.java
@@ -0,0 +1,7 @@
+package frontend.highscoreDisplay;
+
+import javax.swing.JFrame;
+
+public class HighscoreList extends JFrame {
+
+}
